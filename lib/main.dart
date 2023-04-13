e49bca224d3549030e800fe54be170545123085f
Hashir Rajah <mhashirhrajah00@gmail.com>
2023-04-13 13:44:06 +0400

    Little Tweaks

--- lib/main.dart
+++ lib/main.dart
@@ -14,5 +14,5 @@ Future<void> main() async {
   // make status bar transparent
   //changeStatusBarColorToTransparent();
   // run application
-  runApp(const MyApp());
+  runApp(const Tripplanner());
 }
