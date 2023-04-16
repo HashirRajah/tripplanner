19f8135888aa186d411223247f514428eb076212
Hashir Rajah <mhashirhrajah00@gmail.com>
2023-04-16 14:14:25 +0400

    Internet Connection Check

--- lib/root.dart
+++ lib/root.dart
@@ -3,10 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tripplanner/screens/wrapper_screen/wrapper_screen.dart';
 import 'shared/constants/theme_constants.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 
 class Tripplanner extends StatelessWidget {
   //
   final _auth = FirebaseAuth.instance;
+  final InternetConnectionChecker _internetConnectionChecker =
+      InternetConnectionChecker();
   //
   Tripplanner({super.key});
   //
@@ -19,6 +22,10 @@ class Tripplanner extends StatelessWidget {
         StreamProvider<User?>.value(
           value: _auth.userChanges(),
           initialData: _auth.currentUser,
+        ),
+        StreamProvider<InternetConnectionStatus?>.value(
+          value: _internetConnectionChecker.onStatusChange,
+          initialData: null,
         ),
       ],
       child: MaterialApp(
--- lib/screens/wrapper_screen/wrapper_screen.dart
+++ lib/screens/wrapper_screen/wrapper_screen.dart
@@ -4,6 +4,8 @@ import 'package:provider/provider.dart';
 import 'package:tripplanner/screens/auth_navigation_screen/auth_navigation_screen.dart';
 import 'package:tripplanner/screens/email_verification_screen/email_verification_screen.dart';
 import 'package:tripplanner/screens/home_screens/home.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:tripplanner/screens/network_error_screen/network_error_screen.dart';
 
 class WrapperScreen extends StatelessWidget {
   const WrapperScreen({super.key});
@@ -17,9 +19,20 @@ class WrapperScreen extends StatelessWidget {
     //
     final User? user = Provider.of<User?>(context);
     debugPrint(user?.displayName);
+    // check internet connection
+    final InternetConnectionStatus? connectionStatus =
+        Provider.of<InternetConnectionStatus?>(context);
     // user not logged in
     if (user == null) {
-      return const AuthNavigationScreen();
+      if (connectionStatus == InternetConnectionStatus.connected) {
+        return const AuthNavigationScreen();
+      }
+      //
+      WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+        Navigator.of(context).popUntil((route) => route.isFirst);
+      });
+      //
+      return const NetworkErrorScreen();
     }
     // if user did not verify email
     final bool verifiedUser = user.emailVerified;
